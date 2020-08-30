9933b7521703cbca2149011710640d08f107e8e8
tangmingkao <1479530837@qq.com>
2020-08-30 18:35:32 +0800

    update

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import babel from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve';
+
+export default {
+    input: './src/index.js', //入口文件
+    output: {
+        format: 'umd', //模块化类型 esModule commonjs
+        name: 'Vue',
+        file: 'dist/umd/vue.js',
+        sourcemap: true,
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        serve({
+            open: true,
+            //浏览器端口
+            port: 3000,
+            contentBase: '',
+            openPage: '/index.html'
+        }),
+    ],
+}
